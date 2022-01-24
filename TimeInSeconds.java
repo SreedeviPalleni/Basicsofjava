abc2d3e95384eaad3c7b3424121286b4029c6e46
SreedeviPalleni <97502846+SreedeviPalleni@users.noreply.github.com>
2022-01-24 08:31:01 +0530

    Add files via upload

---
+++ TimeInSeconds.java
@@ -0,0 +1,16 @@
+package timeproject;
+
+public class TimeInSeconds {
+	public static void main(String args[])
+	{
+	
+	int hour=24*60;
+	int min=hour*60;
+	int sec=min*60;
+	System.out.println(sec);
+	}
+	
+
+	
+
+}
