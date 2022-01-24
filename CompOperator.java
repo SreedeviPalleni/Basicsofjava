5bd76b74973cf2a0bd27f86776809918df456355
SreedeviPalleni <97502846+SreedeviPalleni@users.noreply.github.com>
2022-01-24 08:20:31 +0530

    Add files via upload

---
+++ CompOperator.java
@@ -0,0 +1,25 @@
+package sreedevi;
+
+public class CompOperator {
+
+	public static void main(String[] args) 
+	{
+		
+	int a =9, b=10 ;
+	boolean c;
+	c=a>b;
+	System.out.println(c);
+	c=a<b;
+	System.out.println(c);
+	c=a>=b;
+	System.out.println(c);
+	c=a<=b;
+	System.out.println(c);
+	
+	
+	
+	
+
+	}
+
+}
